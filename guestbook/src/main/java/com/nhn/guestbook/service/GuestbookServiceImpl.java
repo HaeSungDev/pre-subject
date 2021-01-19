5ae4b4441560eace0fe9d01af9c1baaf3c1a2035
HaeSung <haesung.dev@gmail.com>
2021-01-19 16:00:58 +0900

    add guestbook service tests. #9

--- guestbook/src/main/java/com/nhn/guestbook/service/GuestbookServiceImpl.java
+++ guestbook/src/main/java/com/nhn/guestbook/service/GuestbookServiceImpl.java
@@ -2,12 +2,17 @@ package com.nhn.guestbook.service;
 
 import com.nhn.guestbook.dto.GuestbookDTO;
 import com.nhn.guestbook.entity.Guestbook;
+import com.nhn.guestbook.repository.GuestbookRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 @Service
 @Log4j2
+@RequiredArgsConstructor
 public class GuestbookServiceImpl implements GuestbookService {
+    private final GuestbookRepository repository;
+
     @Override
     public Long register(GuestbookDTO dto) {
         log.info("DTO-------------------------");
@@ -17,6 +22,8 @@ public class GuestbookServiceImpl implements GuestbookService {
 
         log.info(entity);
 
-        return null;
+        repository.save(entity);
+
+        return entity.getGno();
     }
 }
