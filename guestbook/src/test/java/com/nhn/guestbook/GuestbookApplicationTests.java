b978fcab6e2f3511b88148a9472ec327a882f579
HaeSung <haesung.dev@gmail.com>
2021-01-19 10:17:59 +0900

    init pre-subject/guestbook project. #1

---
+++ guestbook/src/test/java/com/nhn/guestbook/GuestbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhn.guestbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
