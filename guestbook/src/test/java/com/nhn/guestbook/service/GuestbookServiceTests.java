af85da2d5d8d85db32bab7fa5cf0ca90a1cf9e93
HaeSung <haesung.dev@gmail.com>
2021-01-19 17:23:40 +0900

    add guestbook service list method tests. #10

--- guestbook/src/test/java/com/nhn/guestbook/service/GuestbookServiceTests.java
+++ guestbook/src/test/java/com/nhn/guestbook/service/GuestbookServiceTests.java
@@ -1,6 +1,9 @@
 package com.nhn.guestbook.service;
 
 import com.nhn.guestbook.dto.GuestbookDTO;
+import com.nhn.guestbook.dto.PageRequestDTO;
+import com.nhn.guestbook.dto.PageResultDTO;
+import com.nhn.guestbook.entity.Guestbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,5 +22,19 @@ public class GuestbookServiceTests {
                 .build();
 
         System.out.println(guestbookService.register(guestbookDTO));
+    }
+
+    @Test
+    public void testList() {
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = guestbookService.getList(pageRequestDTO);
+
+        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
+            System.out.println(guestbookDTO);
+        }
     }
 }
