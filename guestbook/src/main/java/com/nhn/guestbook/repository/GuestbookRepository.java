c3f028941b6091aef620692690beb76f89afd5b6
HaeSung <haesung.dev@gmail.com>
2021-01-19 14:51:45 +0900

    add guestbook repository. #8

---
+++ guestbook/src/main/java/com/nhn/guestbook/repository/GuestbookRepository.java
@@ -0,0 +1,8 @@
+package com.nhn.guestbook.repository;
+
+import com.nhn.guestbook.entity.Guestbook;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface GuestbookRepository extends JpaRepository<Guestbook, Long>, QuerydslPredicateExecutor<Guestbook> {
+}
