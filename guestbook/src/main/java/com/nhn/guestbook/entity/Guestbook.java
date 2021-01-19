66eb9c8a218682da7f75a6d4262aa8a59b1c4964
HaeSung <haesung.dev@gmail.com>
2021-01-19 12:25:01 +0900

    add base entity, GuestBook Entity #4

---
+++ guestbook/src/main/java/com/nhn/guestbook/entity/Guestbook.java
@@ -0,0 +1,28 @@
+package com.nhn.guestbook.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Guestbook extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long gno;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Column(length = 1500, nullable = false)
+    private String content;
+
+    @Column(length = 50, nullable = false)
+    private String writer;
+}
