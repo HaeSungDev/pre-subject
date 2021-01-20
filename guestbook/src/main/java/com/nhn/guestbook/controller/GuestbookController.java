aba8b517d124ad04c71ca6e3a7f9a2d3d128d7f2
HaeSung <haesung.dev@gmail.com>
2021-01-20 11:05:18 +0900

    add guestbook read controller. #19

--- guestbook/src/main/java/com/nhn/guestbook/controller/GuestbookController.java
+++ guestbook/src/main/java/com/nhn/guestbook/controller/GuestbookController.java
@@ -8,6 +8,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -44,5 +45,14 @@ public class GuestbookController {
         redirectAttributes.addFlashAttribute("msg", gno);
 
         return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/read")
+    public void read(long gno, @ModelAttribute("requestDTO") PageRequestDTO pageRequestDTO, Model model) {
+        log.info("gno: " + gno);
+
+        GuestbookDTO guestbookDTO = guestbookService.read(gno);
+
+        model.addAttribute("dto", guestbookDTO);
     }
 }
