5b7694b9ec15d82ebfd84c0447d732acaf3a0530
HaeSung <haesung.dev@gmail.com>
2021-01-20 23:24:03 +0900

    [mreview] add member entity. #33

---
+++ mreview/src/main/java/com/nhn/mreview/entity/Member.java
@@ -0,0 +1,26 @@
+package com.nhn.mreview.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@ToString
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mid;
+
+    private String email;
+
+    private String pw;
+
+    private String nickname;
+}
