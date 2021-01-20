ee8e8146d3d75dceaed093a9ab6a009db303a66f
HaeSung <haesung.dev@gmail.com>
2021-01-20 12:03:29 +0900

    add guestbook modify page controller, delete controller. #20

--- guestbook/src/main/java/com/nhn/guestbook/controller/GuestbookController.java
+++ guestbook/src/main/java/com/nhn/guestbook/controller/GuestbookController.java
@@ -47,12 +47,23 @@ public class GuestbookController {
         return "redirect:/guestbook/list";
     }
 
-    @GetMapping("/read")
+    @GetMapping({"/read", "modify"})
     public void read(long gno, @ModelAttribute("requestDTO") PageRequestDTO pageRequestDTO, Model model) {
         log.info("gno: " + gno);
 
         GuestbookDTO guestbookDTO = guestbookService.read(gno);
 
         model.addAttribute("dto", guestbookDTO);
+    }
+
+    @PostMapping("/remove")
+    public String remove(long gno, RedirectAttributes redirectAttributes) {
+        log.info("gno: " + gno);
+
+        guestbookService.remove(gno);
+
+        redirectAttributes.addFlashAttribute("msg", gno);
+
+        return "redirect:/guestbook/list";
     }
 }
