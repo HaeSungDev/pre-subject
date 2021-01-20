153433e1f65cb533d549b315bdb178ea25e121f2
HaeSung <haesung.dev@gmail.com>
2021-01-20 23:21:49 +0900

    [mreview] add MovieImage entity. #33

---
+++ mreview/src/main/java/com/nhn/mreview/entity/MovieImage.java
@@ -0,0 +1,26 @@
+package com.nhn.mreview.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@ToString
+public class MovieImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long inum;
+
+    private String uuid;
+
+    private String imgName;
+
+    private String path;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Movie movie;
+}
