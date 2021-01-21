7cb069381a49d062e93d8c433fe862440b57a9ce
HaeSung <haesung.dev@gmail.com>
2021-01-21 20:25:38 +0900

    [mreview] add movie register controller. #45

--- mreview/src/main/java/com/nhn/mreview/controller/MovieController.java
+++ mreview/src/main/java/com/nhn/mreview/controller/MovieController.java
@@ -1,15 +1,34 @@
 package com.nhn.mreview.controller;
 
+import com.nhn.mreview.dto.MovieDTO;
+import com.nhn.mreview.service.MovieService;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/movie")
 @Log4j2
 public class MovieController {
-    @GetMapping("register")
+    @Autowired
+    MovieService movieService;
+
+    @GetMapping("/register")
     public void register() {
+    }
+
+    @PostMapping("/register")
+    public String register(MovieDTO movieDTO, RedirectAttributes redirectAttributes) {
+        log.info("movieDTO: " + movieDTO);
+
+        Long mno = movieService.register(movieDTO);
+
+        redirectAttributes.addFlashAttribute("msg", mno);
+
+        return "redirect:/movie/list";
     }
 }
