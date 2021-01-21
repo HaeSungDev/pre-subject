808ffcb8d74e1ec4c5669ec2f1bdcf2d63ff0a7d
HaeSung <haesung.dev@gmail.com>
2021-01-21 12:27:46 +0900

    [mreview] add review query and test. #41

--- mreview/src/main/java/com/nhn/mreview/repository/ReviewRepository.java
+++ mreview/src/main/java/com/nhn/mreview/repository/ReviewRepository.java
@@ -1,7 +1,13 @@
 package com.nhn.mreview.repository;
 
+import com.nhn.mreview.entity.Movie;
 import com.nhn.mreview.entity.Review;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    @EntityGraph(attributePaths = {"member"}, type = EntityGraph.EntityGraphType.FETCH)
+    List<Review> findByMovie(Movie movie);
 }
