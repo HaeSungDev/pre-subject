afdac557b52168f06d8a30666ee9ceda6080e996
HaeSung <haesung.dev@gmail.com>
2021-01-21 12:32:58 +0900

    [mreview] add delete review query and test. #41

--- mreview/src/test/java/com/nhn/mreview/repository/MemberRepositoryTests.java
+++ mreview/src/test/java/com/nhn/mreview/repository/MemberRepositoryTests.java
@@ -1,16 +1,22 @@
 package com.nhn.mreview.repository;
 
 import com.nhn.mreview.entity.Member;
+import com.nhn.mreview.entity.Review;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 
+import javax.transaction.Transactional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
 public class MemberRepositoryTests {
     @Autowired
     private MemberRepository memberRepository;
+
+    @Autowired
+    private ReviewRepository reviewRepository;
 
     @Test
     public void insertMembers() {
@@ -22,5 +28,17 @@ public class MemberRepositoryTests {
                     .build();
             memberRepository.save(member);
         });
+    }
+
+    @Commit
+    @Transactional
+    @Test
+    public void testDeleteMember() {
+        Long mid = 2L;
+
+        Member member = Member.builder().mid(mid).build();
+
+        reviewRepository.deleteByMember(member);
+        memberRepository.deleteById(mid);
     }
 }
